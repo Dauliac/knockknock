967c2b3ae3f6851f4418ae20eb08b672689b67d0
tonychg <antoine.chiny@gmail.com>
2018-05-03 00:45:22 +0200

    add gitignore

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,10 +10,15 @@ pos=$PWD
 
 newuser="knockknock"
 newgroups="video,sudo,audio,users"
+uv4l_url="http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc"
+uv4l_deb="deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/strech strech main"
+
 test $(id -u) -eq 0 || exit_err "Permission denied (need root priviliege)"
 echo "Updating System"
+curl $uv4l_url | sudo apt-key add - || exit_err "ERROR: Adding key for webrtc"
+echo $uv4l_deb >> /etc/apt/sources.list || exit_err "ERROR: Adding deb to sources.list"
 apt-get -y update && apt-get -y upgrade || exit_err "ERROR: Update"
-apt-get -y install motion || exit_err "ERROR: Motion install"
+apt-get -y install motion uv4l uv4l-raspicam || exit_err "ERROR: Motion install"
 
 echo "Create user"
 adduser $newuser || exit_err "ERROR: Cannot create $newuser"

7a2c2df2ce31fd38219b06b0594a0675e6d2c7f7
TonyChG <antoine.chiny@gmail.com>
2018-05-29 16:39:37 +0200

    Fix merge
    Signed-off-by: TonyChG <antoine.chiny@gmail.com>

--- app/constants.py
+++ app/constants.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-# coding: utf-8
-"""
-    Author      : github.com/TonyChG
-    Date        : Wed 16 May 2018 03:53:55 AM CEST
-    Description : 
-    Usage       :
-
-"""
 SQL_USER = 'root'
 SQL_DB = 'knockknock'
 SQL_HOST = 'localhost'

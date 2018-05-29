3270ccc923b331aad522c35d5ae49c97783b7f09
Paul-Michel <paulmichel.garzaro@ynov.com>
2018-05-29 11:26:09 +0200

    page login

--- webui/src/components/pages/Login.vue
+++ webui/src/components/pages/Login.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="test">
+    <div class="container">
+      <h1>Login</h1>
+      <form class="form">
+        <input class="form_field" type="text" placeholder="Username">
+        <input class="form_field" type="password" placeholder="Password">
+        <button class="btn" type="submit" id="login-button">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -10,6 +17,43 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped> 
+  .test{
+    height: 800px;
+  }
+  .container{
+    margin: auto;
+    height:20%;
+    width: 50%;
+    border: 0px solid grey;
+    box-shadow: 5px 5px 5px grey;
+    border-radius: .3em;
+    font-family: 'Anton', sans-serif;
+  }
+  .form_field{
+    display: flex;
+    margin:auto;
+    margin-top:1%;
+    border:0;
+    background-color: rgba(145, 145, 145, 0.281);
+    opacity: 7%;
+    font-family: 'Anton', sans-serif;
+  }
+  .btn{
+    margin-top:2%;
+    width: 50%;
+    height: 50%;
+    border: 0;
+    background-color: black;
+    color:white;
+    border-radius: 0.5em;
+    font-family: 'Anton', sans-serif;
+  }
+  .btn:focus{
+    background-color: red;
+  }
+  .container:hover{
+    background-color:rgba(223, 223, 223, 0.281);
+    color:black;
+  }
 </style>

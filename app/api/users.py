112c7dc69f27e829712c2ab9d49bcd942b86b592
TonyChG <antoine.chiny@gmail.com>
2018-05-29 09:28:30 +0200

    Flask project structure
    Signed-off-by: TonyChG <antoine.chiny@gmail.com>

---
+++ app/api/users.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# coding: utf-8
+"""
+    Author      : github.com/TonyChG
+    Date        : Wed 16 May 2018 03:23:09 AM CEST
+    Description : 
+    Usage       :
+
+"""
+
+from app.api import api
+
+@api.route('/users', methods=['GET'])
+def list_users():
+    # List all users
+    pass
+
+@api.route('/users/<int:id>', methods=['GET'])
+def get_user():
+    # Get user by id
+    pass
+
+@api.route('/users/<int:id>', methods=['PUT'])
+def update_user():
+    # Update user by id
+    pass
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    # Create new user
+    pass
+
+@api.route('/users/<int:id>', methods=['DELETE'])
+def delete_user():
+    # Delete user by id
+    pass
+
---
+++ app/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+"""
+    Author      : github.com/TonyChG
+    Date        : Wed 16 May 2018 03:53:55 AM CEST
+    Description : 
+    Usage       :
+
+"""
+
+
